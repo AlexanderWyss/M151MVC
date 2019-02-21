92fdd86fe1b198b1b50df0dec164a0b57107f5b9
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-02-21 10:22:32 +0100

    Hotel in DB

---
+++ MVC/Data/HotelRepositoryDB.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC.Models;
+
+namespace MVC.Data
+{
+    public class HotelRepository
+    {
+        static HotelRepository()
+        {
+        }
+
+        public IEnumerable<Hotel> FindAll()
+        {
+            HotelDBEntities ctx = new HotelDBEntities();
+            return ctx.Hotels;
+        }
+
+        public Hotel FindById(int id)
+        {
+            HotelDBEntities ctx = new HotelDBEntities();
+            ctx.Configuration.LazyLoadingEnabled = false;
+            return ctx
+                .Hotels
+                .FirstOrDefault(h => h.HotelId == id);
+        }
+
+        public void Delete(int hotelid)
+        {
+            var hotel = FindById(hotelid);
+            Delete(hotel);
+        }
+
+        public void Delete(Hotel h)
+        {
+            HotelDBEntities ctx = new HotelDBEntities();
+            ctx.Hotels.Attach(h);
+            ctx.Entry(h).State = System.Data.Entity.EntityState.Deleted;
+            ctx.Hotels.Remove(h);
+            ctx.SaveChanges();
+        }
+
+        public void Save(Hotel h)
+        {
+            HotelDBEntities ctx = new HotelDBEntities();
+            if (h.HotelId == 0)
+            { 
+                ctx.Hotels.Add(h);
+                ctx.SaveChanges();
+            }
+            else
+            {
+                ctx.Hotels.Attach(h);
+                ctx.Entry(h).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
